8c90cc7b84db5db258e14ec74fd037ba1b75107c
Tania Cao <tcaosu@stuy.edu>
2019-05-13 11:51:46 -0400

    add temporary formatting

--- app.py
+++ app.py
@@ -1,75 +1,119 @@
 import os
 
-from flask import Flask, request, render_template, \
-     flash, session, url_for, redirect
+from flask import Flask, render_template, request,flash, session,url_for,redirect
 
 from util import db
 
 app = Flask(__name__)
 
-app.secret_key = os.urandom(32)
+app.secret_key=os.urandom(32)
 
-#---------- Main Page ----------
 @app.route("/")
 def home():
-    if "logged_in" in session:
-        return render_template("home.html", logged_in=True, user=session["logged_in"])
-    else:
-        return render_template("home.html", logged_in=False)
+	if "logged_in" in session:
+		data = db.get_watchlist(session["logged_in"])
+		return render_template("home.html", title = "Home", heading = "Hello " + session["logged_in"] + "!", user = session["logged_in"], logged_in = True, watchilist_data = data)
+	return render_template("home.html", title = "Home", heading = "Hello Guest!", logged_in = False)
 
-#---------- Login/Register----------
+#Authenticates user and adds session
+#Returns to the page the user was on previously
+
+@app.route("/auth", methods = ["GET", "POST"])
+def auth():
+	return_page = "home"
+        
+	for each in request.form:
+		if request.form[each] == "Login":
+			return_page = each
+                        
+	given_user = request.form["username"]
+	given_pwd = request.form["password"]
+	if db.auth_user(given_user, given_pwd):
+		session["logged_in"] = given_user
+		return redirect(url_for(return_page))
+	else:
+		flash("Username or password is incorrect")
+		return redirect(url_for("login"))
+        
 @app.route("/login")
 def login():
-    if "logged_in" in session:
-        return redirect(url_for("home"))
-    return render_template("login.html")
+	return render_template("login.html", title = "Login", heading = "Login", type = "home")
 
-@app.route("/auth")
-def authenticate():
-    try:
-        if db.auth_user(request.args["user"], request.args["password"]):
-            session["logged_in"] = request.args["user"]
-            return redirect(url_for("home"))
-        else:
-            flash("username or password is incorrect")
-            return redirect(url_for("login"))
-    except:
-        flash("Something went wrong :(")
-        return redirect(url_for("home"))
-
+#Sends the user to the register.html to register a new account
 @app.route("/register")
 def register():
-    return render_template("register.html")
+	return render_template("register.html", title = "Register", heading = "Register")
 
+#Attempts to add the user to the database
 @app.route("/adduser")
 def add_user():
-    try:
-        if(not request.args["user"].strip() or not request.args["password"] or not request.args["confirm_password"]):
+    if(not request.args["user"].strip() or not request.args["password"] or not request.args["confirm_password"]):
             flash("Please fill in all fields")
             return redirect(url_for("register"))
 
-        if(db.check_user(request.args["user"])):
+    if(db.user_exist(request.args["user"])):
             flash("User already exists")
             return redirect(url_for("register"))
 
-        if(request.args["password"] != request.args["confirm_password"]):
+    if(request.args["password"] != request.args["confirm_password"]):
             flash("Passwords don't match")
             return redirect(url_for("register"))
 
-        db.add_user(request.args["user"], request.args["password"])
-        session["logged_in"] = request.args["user"]
-    except:
-        flash("Something went wrong :(")
+    db.add_user(request.args["user"], request.args["password"])
+    session["logged_in"] = request.args["user"]
     return redirect(url_for("home"))
 
-#---------- Logout ----------
+#Logs the user out and removes session
+#returns to the page the user was on previously
 @app.route("/logout")
 def logout():
-    try:
-        session.pop("logged_in")
-    finally:
-        return redirect(url_for("home"))
+	if session.get("logged_in"):
+		session.pop("logged_in")
+		print(session)
+	return redirect(url_for("home"))
+
+# Depending on the current status, either adds or removes from the watchlist
+# Returns to the original search page.
+@app.route("/changeWatchlist", methods = ["GET", "POST"])
+def changeWatchlist():
+	print ("request.args: " )
+	print ( request.args )
+	print ( "\n ------------")
+
+	for each in request.args:
+		if request.args[each] == "Add to watchlist":
+			data = each.split("{!{!!}!}")
+			search = data[1]
+			companyCode = data[0].lower()
+			db.add_watchlist(session["logged_in"], companyCode)
+		if request.args[each] == "Remove from watchlist":
+			data = each.split("{!{!!}!}")
+			search = data[1].replace("|~|~|", " ")
+			companyCode = data[0].lower()
+			db.remove_watchlist(session["logged_in"], companyCode)
+	return redirect(url_for("stockResults", stock_info = search))
+
+@app.route("/removeWatchlist", methods = ["GET", "POST"])
+def removeWatchlist():
+	print(request.args)
+	for each in request.args:
+		if request.args[each] == "Remove from watchlist":
+			data = each.split("{!{!!}!}")
+			companyCode = data[0]
+			db.remove_watchlist(session["logged_in"], companyCode)
+	return redirect(url_for("watchlist"))
+
+@app.route("/watchlist")
+def watchlist():
+	if "logged_in" in session:
+		watchlist_data = db.get_watchlist(session["logged_in"])
+		for each in watchlist_data:
+			each[0] = info.getStocks(each[1].lower())
+		return render_template("watchlist.html", watchlist = watchlist_data, title = "Watchlist", heading = "Watchlist", logged_in = True)
+	else:
+		flash ("Please login to view Watchlist")
+		return render_template("login.html", title = "Login", heading = "Login", type = "watchlist")#redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+        app.debug = True
+        app.run()
--- util/db.py
+++ util/db.py
@@ -1,53 +1,108 @@
 import sqlite3
-DB_FILE="./data/watchlist.db"
+DB_FILE = "data/info.db"
 
-# ===================== Initiate =====================
-def createTable():
-    """Create all data tables."""
-    db = sqlite3.connect(DB_FILE)
+def create_tables():
+    """Creates tables for users' info, portfolios, users' stocks and watchlist"""
+    db = sqlite3.connect("../" + DB_FILE)
     c = db.cursor()
-
-    command = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)"
+    command = "CREATE TABLE user_info (username TEXT, password TEXT)"
     c.execute(command)
 
-    command = "CREATE TABLE watchlist (username TEXT PRIMARY KEY, location TEXT)"
+    command = "CREATE TABLE watchlist (username TEXT, stock_name TEXT)"
     c.execute(command)
 
-    db.commit()
-    db.close()
+    db.commit() #save changes
+    db.close()  #close database
 
-# createTable()
-
-# ===================== User Accounts =====================
+#login / register routes
 def add_user(username, password):
-    """Insert credentials for newly registered user into database."""
+    """Insert the credentials for newly registered users into the database"""
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("INSERT INTO users VALUES(?, ?)", (username, password))
+    c.execute("INSERT INTO user_info VALUES(?, ?)", (username, password))
     db.commit() #save changes
     db.close()  #close database
 
 def auth_user(username, password):
-    """Authenticate a user attempting to log in."""
+    """Authenticate user attempting to log in"""
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-
-    # user_info = c.execute("SELECT users.username, users.password FROM users WHERE username={} AND password={}".format(username, password))
-    for entry in c.execute("SELECT users.username, users.password FROM users"):
-        if(entry[0] == username and entry[1] == password):
+    for each in c.execute("SELECT user_info.username, user_info.password FROM user_info"):
+        if(each[0] == username and each[1] == password):
             db.close()
             return True
     db.close()
     return False
 
-def check_user(username):
+def user_exist(username):
     """Check if a username has already been taken when registering."""
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
-    for entry in c.execute("SELECT users.username FROM users"):
-        if(entry[0] == username):
+    for each in c.execute("SELECT user_info.username FROM user_info"):
+        if(each[0] == username):
             db.close()
             return True
     db.close()
     return False
+
+def add_watchlist(user, new_watchlist):
+    """Inserts a new watchlist stock into the db for a users watchlist"""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    if (check_watchlist(user, new_watchlist)):
+        db.close()
+        return False
+    c.execute("INSERT INTO watchlist VALUES(?, ?)", (user, new_watchlist))
+
+    db.commit()
+    db.close()
+    return True
+
+def rmv_user(user):
+    """Remove the portfolio info of user"""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    c.execute("DELETE FROM portfolio WHERE username = '{}'".format(user))
+
+    db.commit()
+    db.close()
+
+def remove_watchlist(user, rmv_watchlist_name):
+    """Remove the stock rmv_watchlist_name from the watchlist for user."""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    c.execute("DELETE FROM watchlist WHERE username = '{}' and stock_name = '{}'".format(user, rmv_watchlist_name))
+
+    db.commit()
+    db.close()
+
+def check_watchlist(user, company_name):
+    """Check to see if the user already has the stock in the watchlist."""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    for each in c.execute("SELECT watchlist.stock_name FROM watchlist WHERE username = '{}'".format(user)):
+        if(each[0] == company_name):
+            db.close()
+            return True
+
+    db.close()
+    return False
+
+def get_watchlist(user):
+    """Gets all the watchlist stocks for the user."""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    watchlist_data = []
+    ret_val = c.execute("SELECT watchlist.stock_name FROM watchlist WHERE username = '{}'".format(user))
+    for each in ret_val:
+        companyCode = each[0]
+        watchlist_data.append([0, companyCode])
+
+    db.close()
+    return watchlist_data
