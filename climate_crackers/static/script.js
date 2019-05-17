5af07733ee45aaa87e99a57331ded468ec8f9c34
Puneet Johal <pjohal@stuy.edu>
2019-05-17 12:26:09 -0400

    added map

--- climate_crackers/static/script.js
+++ climate_crackers/static/script.js
@@ -1,1 +1,21 @@
-//d3 stuff
+console.log("script loaded")
+
+var svg = d3.select("svg");
+
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "county-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+
+});
