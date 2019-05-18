560ff7db1daf807c4c4cc617d7f9468c01d4f90d
Puneet Johal <pjohal@stuy.edu>
2019-05-18 12:24:02 -0400

    choropleth map setup

--- climate_crackers/static/script.js
+++ climate_crackers/static/script.js
@@ -4,7 +4,7 @@ var svg = d3.select("svg");
 
 var path = d3.geoPath();
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+d3.json("https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json", function(error, us) {
   if (error) throw error;
 
   svg.append("g")
@@ -12,10 +12,26 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .enter().append("path")
-      .attr("d", path);
+      .attr("d", path)
+      .attr("id", name)
+      .on("click", function(d) {
+          console.log(d.properties.name)
+      });
+  console.log(us)
 
   svg.append("path")
       .attr("class", "county-borders")
       .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 
 });
