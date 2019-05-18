9041321490919f883259fb807cfaf93ffaebd1b5
Puneet Johal <pjohal@stuy.edu>
2019-05-18 12:57:20 -0400

    cleanup and favicon

--- climate_crackers/static/script.js
+++ climate_crackers/static/script.js
@@ -1,9 +1,8 @@
-console.log("script loaded")
-
 var svg = d3.select("svg");
 
 var path = d3.geoPath();
 
+//JSON file being used here is an online version of the map.json file in the data directory. We aren't using the local version to bypass Chrome errors with opening a local file.
 d3.json("https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json", function(error, us) {
   if (error) throw error;
 
@@ -12,12 +11,13 @@ d3.json("https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json", function(error, u
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .enter().append("path")
+      .attr("id", function(d) {
+        return d.properties.name;
+      })
       .attr("d", path)
-      .attr("id", name)
       .on("click", function(d) {
-          console.log(d.properties.name)
+          console.log(this)
       });
-  console.log(us)
 
   svg.append("path")
       .attr("class", "county-borders")
