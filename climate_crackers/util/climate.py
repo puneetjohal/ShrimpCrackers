4f52c0d2b29a7a6af56d950d4da72bcebd2a04e4
Joyce Liao <jycqliao@gmail.com>
2019-05-29 12:56:47 -0400

    climate loop fix

--- climate_crackers/util/climate.py
+++ climate_crackers/util/climate.py
@@ -9,37 +9,43 @@ token = {"token": "jggiGITnyOHqgrVCGTgWCMycNLzIchHJ"}
 #===============================================================================
 
 def getcityid(city, state):
+    print("getting city id")
     offset = 0
     for x in range(0,2):#total: 1988 cities
         url = "http://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&limit=1000&offset=" + str(offset)
         req = urllib.request.Request(url, data=None, headers=token)
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
-        print(len(data['results']))
+        # print(len(data['results']))
         for x in range(0, len(data['results'])):
             # print(data['results'][x]['name'])
             if city in data['results'][x]['name'] and state in data['results'][x]['name']:
                 print(data['results'][x]['id'])
                 return data['results'][x]['id']
         offset += 1000
-    print("NOT FOUND")
+    # print("NOT FOUND")
     return "NOT FOUND"
 
 # getcityid("Fort Myers", "TX")
 # getcityid("Salt Lake City", "UT")
+# print(getcityid("Brooklyn", "NY"))
 
 def getcountyid(county, state):
+    print("getting county id")
     offset = 0
     for x in range(0, 4):#total: 3,179 counties
         url = "http://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CNTY&limit=1000&offset=" + str(offset)
         req = urllib.request.Request(url, data=None, headers=token)
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
-        print(len(data['results']))
+        # print(len(data['results']))
         for i in range(0, len(data['results'])):
-            if county in data['results'][x]['name'] and state in data['results'][x]['name']:
-                print(data['results'][x]['id'])
-                return data['results'][x]['id']
+            # print(data['results'][x]['name'])
+            # if state in data['results'][i]['name']:
+            #     print(data['results'][i]['name'])
+            if county in data['results'][i]['name'] and state in data['results'][i]['name']:
+                print(data['results'][i]['id'])
+                return data['results'][i]['id']
         offset += 1000
     print("NOT FOUND")
     return "NOT FOUND"
@@ -47,7 +53,7 @@ def getcountyid(county, state):
 def getSearchInfo(city, county, state):
     ID = getcityid(city, state)
     if ID == "NOT FOUND":
-        ID = getcountyid()
+        ID = getcountyid(county, state)
     stations = ""
     url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?datatypeid=TAVG&locationid=" + ID
     req = urllib.request.Request(url, data=None, headers=token)
@@ -60,14 +66,18 @@ def getSearchInfo(city, county, state):
         stations += s + ","
     #remove trailing comma
     stations = stations[:-1]
-    print(stations)
+    # print(stations)
     url = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=TAVG&stations=" + stations + "&startDate=1900-01-01&endDate=2018-12-31&format=json&units=standard"
     req = urllib.request.Request(url, data=None, headers=token)
     response = urllib.request.urlopen(req)
     data = json.loads(response.read())
-    return data
+    ret_data = []
+    for entry in data:
+        if 'TAVG' in entry:
+            ret_data.append(entry)
+    return ret_data
 
-
+# print(getSearchInfo("New York City", "Kings County", "NY"))
 #===============================================================================
 # COUNTY WEATHER INFORMATION FOR LANDING PAGE
 #===============================================================================
