1a332cc587e2f2bad97971ca0e6e5dd660b2da84
sxia1 <sxia1@stuy.edu>
2019-05-29 14:24:44 -0400

    fxn writes to json file

--- climate_crackers/util/climate.py
+++ climate_crackers/util/climate.py
@@ -90,7 +90,7 @@ def getCountyID():
     cntyIDs = {}
     offset = 0
     for x in range(0, 4):#total: 3,179 counties
-        url = "http://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CNTY&limit=1000&offset=" + str(offset)
+        url = "http://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CNTY&limit=10&offset=" + str(offset)
         req = urllib.request.Request(url, data=None, headers=token)
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
@@ -110,25 +110,28 @@ def getCntyInfo():
         req = urllib.request.Request(url, data=None, headers=token)
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
-        for i in range(0, len(data['results'])):
-            s = data['results'][i]['id']
-            #remove the colon and everything before it
-            s = s.split(':', 1)[-1]
-            stations += s + ","
-        #remove trailing comma
-        stations = stations[:-1]
-        #get the info
-        url = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=TAVG&stations=" + stations + "&startDate=1900-01-01&endDate=2018-12-31&format=json&units=standard"
-        req = urllib.request.Request(url, data=None, headers=token)
-        response = urllib.request.urlopen(req)
-        data = json.loads(response.read())
-        alist = []
-        for i in range(0, len(data)):
-            #INSERT INFO
-            if 'TAVG' in data[i]:
-                alist.append(data[i]['DATE'])
-                alist.append(data[i]['TAVG'])
-        info[x] = alist
-        print(x, alist)
+        if 'results' in data:
+            for i in range(0, len(data['results'])):
+                s = data['results'][i]['id']
+                #remove the colon and everything before it
+                s = s.split(':', 1)[-1]
+                stations += s + ","
+            #remove trailing comma
+            stations = stations[:-1]
+            #get the info
+            url = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=TAVG&stations=" + stations + "&startDate=1900-01-01&endDate=2018-12-31&format=json&units=standard"
+            req = urllib.request.Request(url, data=None, headers=token)
+            response = urllib.request.urlopen(req)
+            data = json.loads(response.read())
+            alist = []
+            for j in range(0, len(data)):
+                if 'TAVG' in data[j]:
+                    alist.append({data[j]['DATE']:data[j]['TAVG']})
+                else:
+                    alist.append({data[j]['DATE']: ""})
+            info[x] = alist
+    with open('tavg.json', 'w') as outfile:
+    json.dump(info, outfile)
 
-#getCntyInfo()
+getCntyInfo()
+
