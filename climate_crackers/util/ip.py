998f8b0b60f65f4b542ac8a96cd2161aa6c1eb7d
Joyce Liao <jycqliao@gmail.com>
2019-06-02 17:50:26 -0400

    request

--- climate_crackers/util/ip.py
+++ climate_crackers/util/ip.py
@@ -6,7 +6,8 @@ import json
 KEY = "505ea9a7b91d5ca93f52056ca5bf77fd"
 
 def get_coord():
-    IP = json.loads(urllib.request.urlopen("http://ip.jsontest.com/").read())['ip']
+    req = urllib.request.Request("http://ip.jsontest.com/")
+    IP = json.loads(urllib.request.urlopen(req).read())['ip']
     URL = "http://api.ipstack.com/" + IP +"?access_key=" + KEY + "&output=json"
     req = urllib.request.Request(URL)
     response = urllib.request.urlopen(req)
