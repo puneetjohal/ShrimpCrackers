ea470d43c077b70baabae5257361bafe07e2fac3
Puneet Johal <pjohal@stuy.edu>
2019-06-11 07:12:40 -0400

    legend

--- climate_crackers/static/script.js
+++ climate_crackers/static/script.js
@@ -1,61 +1,8 @@
 var svg = d3.select("#landing_map");
+var mapLegend = d3.select("#mapLegend");
 var placeholder = d3.select("#loading")
 
-var w = 320, h = 50;
-
 var path = d3.geoPath();
-
-var key = d3.select("#legend");
-key.style("padding-top", "20px")
-  .style("height", "80px");
-
-var legend = key.append("defs")
-      .append("svg:linearGradient")
-      .attr("id", "gradient")
-      .attr("x1", "0%")
-      .attr("y1", "100%")
-      .attr("x2", "100%")
-      .attr("y2", "100%")
-      .attr("spreadMethod", "pad");
-
-legend.append("stop")
-  .attr("offset", "0%")
-  .attr("stop-color", "steelblue")
-  .attr("stop-opacity", 1);
-
-// legend.append("stop")
-//   .attr("offset", "33%")
-//   .attr("stop-color", "#bae4bc=")
-//   .attr("stop-opacity", 1);
-//
-// legend.append("stop")
-//   .attr("offset", "66%")
-//   .attr("stop-color", "#7bccc4")
-//   .attr("stop-opacity", 1);
-
-legend.append("stop")
-  .attr("offset", "100%")
-  .attr("stop-color", "red")
-  .attr("stop-opacity", 1);
-
-key.append("rect")
-  .attr("width", w - 20)
-  .attr("height", h - 30)
-  .style("fill", "url(#gradient)")
-  .attr("transform", "translate(10,10)");
-
-var y = d3.scaleLinear()
-  .range([310, 10])
-  .domain([100, 0]);
-
-var yAxis = d3.axisBottom()
-  .scale(y)
-  .ticks(5);
-
-key.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(0,30)")
-  .call(yAxis);
 
 //Year display on HTML
 var slider = document.getElementById("year")
@@ -187,3 +134,48 @@ d3.json("https://raw.githubusercontent.com/puneetjohal/ShrimpCrackers/master/cli
   update(2010);
   placeholder.remove();
 }); //Close temps JSON
+
+//Add legend
+var w = 140, h = 300;
+
+var key = mapLegend
+	.append("g")
+	.attr("width", w)
+	.attr("height", h)
+	.attr("class", "legend");
+
+var legend = key.append("defs")
+	.append("svg:linearGradient")
+	.attr("id", "gradient")
+	.attr("x1", "100%")
+	.attr("y1", "0%")
+	.attr("x2", "100%")
+	.attr("y2", "100%")
+	.attr("spreadMethod", "pad");
+
+legend.append("stop")
+	.attr("offset", "0%")
+	.attr("stop-color", "red")
+	.attr("stop-opacity", 1);
+
+legend.append("stop")
+	.attr("offset", "100%")
+	.attr("stop-color", "steelblue")
+	.attr("stop-opacity", 1);
+
+key.append("rect")
+	.attr("width", w - 100)
+	.attr("height", h)
+	.style("fill", "url(#gradient)")
+	.attr("transform", "translate(0,10)");
+
+var y = d3.scaleLinear()
+	.range([h, 0])
+	.domain([15, 100]);
+
+var yAxis = d3.axisRight(y);
+
+key.append("g")
+	.attr("class", "y axis")
+	.attr("transform", "translate(41,10)")
+	.call(yAxis);
